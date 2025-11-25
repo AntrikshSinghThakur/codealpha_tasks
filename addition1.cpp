c7f7a3917b8636a7f734ec4e34d1aa4cf2a37315
AntrikshSinghThakur <thakurantrikshsingh50@gmail.com>
2025-11-25 21:10:48 +0530

    Delete addition1.cpp

--- addition1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main () { int a ,b;
-std::cout << "Enter two numbers:";
-std:: cin >> a >> b;
-int sum = a + b;       //Declaration of sum
-
-std::cout << "sum of " <<  a << " and " << b <<" is: " << sum << std:: endl;
-return 0;
-}
